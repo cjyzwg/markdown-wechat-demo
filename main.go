672e8aaeee09eb06c96dca1f611a634f54489ce5
cj <oncwnuIj9nX593ZY59VZik65lFMY@git.weixin.qq.com>
2022-03-10 18:07:24 +0800

    更新demo

---
+++ main.go
@@ -0,0 +1,38 @@
+package demo
+
+import (
+	"fmt"
+
+	"github.com/cjyzwg/markdown-wechat/draft"
+	"github.com/fastwego/offiaccount"
+	"github.com/spf13/viper"
+)
+
+var App *offiaccount.OffiAccount
+
+func init() {
+	viper.SetConfigFile(".env")
+	_ = viper.ReadInConfig()
+
+	config := offiaccount.Config{
+		Appid:  viper.GetString("APPID"),
+		Secret: viper.GetString("SECRET"),
+	}
+	App = offiaccount.New(config)
+}
+
+func test() {
+
+	config_file := &draft.ConfigFile{
+		MarkdownFilePath: "demo/assets/test.md",
+		CssFilePath:      "demo//asstes/xzdnf-wechat.css",
+		ImagePath:        "demo/assets/img.jpg",
+		Title:            "test",
+		Author:           "c",
+		Digest:           "c",
+	}
+
+	res, err := draft.DraftRun(config_file, App)
+	fmt.Println(res, err)
+
+}
